24776d39c0c6b5d0e88c31682fae33067c3f2831
David Miotti <david@muxumuxu.com>
2018-04-18 22:01:08 +0200

    [+] Added development seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias ChessEarth.Accounts
+alias ChessEarth.Events
+
+{:ok, david} = Accounts.create_user(%{name: "David Miotti", email: "david@muxumuxu.com", password: "muxumuxu"})
+{:ok, audrey} = Accounts.create_user(%{name: "Audrey Labuxiere", email: "audrey@muxumuxu.com", password: "muxumuxu"})
+
+for _ <- 1..10 do
+  Events.create_event(%{
+    name: Faker.Lorem.sentence,
+    start_at: Timex.now,
+    end_at: Timex.shift(Timex.now, days: 3),
+    user_id: [david.id, audrey.id] |> Enum.take_random(1) |> hd
+  })
+end
